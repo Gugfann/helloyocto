8c738c51b47c43e782cf446cf674e909d285d6b7
Gugfann <kmolha@gmail.com>
2023-06-07 15:03:03 +0200

    Update source code again

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 int main(){
     std::cout << "Hello, Yocto..." << std::endl;
     
-    std::cout << "There is more to say here" << std::endl;
+    std::cout << "There is more to say here..." << std::endl;
+    
+    std::cout << "It just keeps coming..." << std::endl;
 
     return 0;
 }
